7e71b0c4d034a457ee87154d79d83c3b94f9be22
wuwenbin <wenbin.wu@foxmail.com>
2019-03-14 17:42:17 +0800

    Update

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PWD', '');
+define('DB_NAME', 'favorite');
+define('DB_PREFIX', 'pre_');
---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r config.php
+touch config.php
+chmod 777 config.php
+rm -r favorite/Runtime
+mkdir favorite/Runtime
+chmod 777 favorite/Runtime
